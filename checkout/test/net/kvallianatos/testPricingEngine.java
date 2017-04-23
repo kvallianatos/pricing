16a18c6d968fc2ac1928a171763739c37a85989e
kvallianatos <kvallianatos@gmail.com>
2017-04-23 20:10:23 +0100

    Added randomized test.

--- checkout/test/net/kvallianatos/testPricingEngine.java
+++ checkout/test/net/kvallianatos/testPricingEngine.java
@@ -7,6 +7,9 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,24 +21,62 @@ public class testPricingEngine {
 	 */
 	@Test
 	public void testCalculateShoppingCartCost() {
-		ShoppingCart cart = createCart(3);
+		ShoppingCart cart = createStaticCart();
+		assertEquals("Incorrect final charge calculated", BigDecimal.valueOf(7.37), new CheckoutService().handleCheckout(cart));
 
-		assertEquals("Incorrect final charge calculated", BigDecimal.valueOf(4.18), new CheckoutService().handleCheckout(cart));
+		TestingCartTuple cartTuple = createRandomCart(1000);
+		assertEquals("Incorrect final charge calculated", cartTuple.total, new CheckoutService().handleCheckout(cartTuple.cart));
 	}
 
-	private ShoppingCart createCart(int numItems) {
+	private ShoppingCart createStaticCart() {
+		ShoppingCart cart = new ShoppingCart();
+
+		cart.getCartItems().put("Beans", new ArrayList<>());
+		cart.getCartItems().put("Coke", new ArrayList<>());
+		cart.getCartItems().put("Milk", new ArrayList<>());
+
+		cart.getCartItems().get("Beans").add(new Item("Beans", BigDecimal.valueOf(1.99)));
+		cart.getCartItems().get("Beans").add(new Item("Beans", BigDecimal.valueOf(1.99)));
+		cart.getCartItems().get("Coke").add(new Item("Coke", BigDecimal.valueOf(0.99)));
+		cart.getCartItems().get("Milk").add(new Item("Milk", BigDecimal.valueOf(1.20)));
+		cart.getCartItems().get("Milk").add(new Item("Milk", BigDecimal.valueOf(1.20)));
+
+		return cart;
+	}
+
+	private TestingCartTuple createRandomCart(int numItems) {
+		Random r = new Random();
+		BigDecimal total = BigDecimal.ZERO;
+
+		String[] itemTypes = {"Beans", "Coke", "Milk", "Orange juice", "Eggs", "Flour", "Avocado", "Wine"};
+		Map<String, BigDecimal> itemPrices = new HashMap<>();
+		for (int i = 0; i < itemTypes.length; i++) {
+			itemPrices.put(itemTypes[i], BigDecimal.valueOf(r.nextDouble() * (double)r.nextInt(10)).setScale(2, BigDecimal.ROUND_DOWN));
+		}
+
+		TestingCartTuple cartTuple = new TestingCartTuple();
 		ShoppingCart cart = new ShoppingCart();
 
 		for(int i = 0; i < numItems; i++) {
-			cart.getCartItems().put("Beans", new ArrayList<>());
-			cart.getCartItems().put("Coke", new ArrayList<>());
-			cart.getCartItems().put("Milk", new ArrayList<>());
+			String itemType = itemTypes[r.nextInt(itemTypes.length)];
+			total = total.add(itemPrices.get(itemType));
 
-			cart.getCartItems().get("Beans").add(new Item("Beans", BigDecimal.valueOf(1.99)));
-			cart.getCartItems().get("Coke").add(new Item("Coke", BigDecimal.valueOf(0.99)));
-			cart.getCartItems().get("Milk").add(new Item("Milk", BigDecimal.valueOf(1.20)));
+			if (cart.getCartItems().containsKey(itemType)) {
+				cart.getCartItems().get(itemType).add(new Item(itemType, itemPrices.get(itemType)));
+			} else {
+				ArrayList<Item> itemList = new ArrayList<>();
+				itemList.add(new Item(itemType, itemPrices.get(itemType)));
+				cart.getCartItems().put(itemType, itemList);
+			}
 		}
 
-		return cart;
+		cartTuple.total = total;
+		cartTuple.cart = cart;
+		return cartTuple;
+	}
+
+	class TestingCartTuple {
+		BigDecimal total;
+		ShoppingCart cart;
 	}
 }
